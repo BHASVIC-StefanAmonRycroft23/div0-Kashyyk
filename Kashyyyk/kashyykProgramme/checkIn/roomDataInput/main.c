0b1eebd12935900695de16d5e9f12cd6958d9f21
BHASVIC-DylanStowell23 <145544472+BHASVIC-DylanStowell23@users.noreply.github.com>
2023-11-03 11:47:39 +0000

    Initialised variables for user input

--- Kashyyyk/kashyykProgramme/checkIn/roomDataInput/main.c
+++ Kashyyyk/kashyykProgramme/checkIn/roomDataInput/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    char fName[10], sName[10];
+    char DoB[9];
+    char bType[3];
+    int numChild;
+    int numAdult;
+    int lengthStay;
+    int newspaper;
+
+    printf("Enter Your Full Name: \n");
+    scanf("%c%c", fName, sName)
     return 0;
 }
