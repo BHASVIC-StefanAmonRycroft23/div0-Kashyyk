09df3442d978a64576d1627a8b0f171794320d61
BHASVIC-StefanAmonRycroft23 <stefan.amonrycroft23@students.bhasvic.ac.uk>
2023-11-06 13:03:11 +0000

    jyghk.jghj

---
+++ Kashyyyk/databaseManagement/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void roomDbEnter(int room);
+void roomDbClear(int room);
+void roomDbRead(int room);
+int roomDbCheck();
+
+int roomNo;
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+void roomDbEnter(int room){
+
+}
+
+void roomDbClear(int room){
+
+}
+
+void roomDbRead(int room){
+
+}
+
+int roomDbCheck(){
+    
+}
